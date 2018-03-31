5eb0680a61127d6a450b9e563874b48847416eca
TheAlexhuff <36049215+TheAlexhuff@users.noreply.github.com>
2018-03-31 16:54:06 -0400

    Add files via upload

---
+++ Alexander Epilon/UnnamedRPG/Scripts/HeartCollect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartCollect : MonoBehaviour {
+    public int rotateSpeed;
+    public AudioSource collectSound;
+    public GameObject thisHeart;
+	
+	// Update is called once per frame
+	void Update () {
+        rotateSpeed = 2;
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+	}
+    void onTriggerEnter ()
+    {
+        collectSound.Play ();
+        thisHeart.SetActive(false);
+    }
+
+
+
+}
