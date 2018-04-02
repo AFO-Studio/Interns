39fa0fab10dea0c6de0c357c6481b085519c880d
Russell Hapney <36056999+russhapney6@users.noreply.github.com>
2018-04-02 01:35:14 -0400

    Add files via upload

---
+++ Russell Hapney/HUDScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour {
+
+    float playerScore = 0;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        playerScore += Time.deltaTime;	
+	}
+
+    public void IncreaseScore(int amount)
+    {
+        playerScore += amount;
+    }
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));
+    }
+}
