7b07de09050a211118ac0792eaf035fcc9dc4eaa
Cory Brown <17816149+CoryBrown-42@users.noreply.github.com>
2018-04-04 22:48:09 -0400

    Edits to the script,
     done in web browser so let me know if no work

--- Russell Hapney/HUDScript.cs
+++ Russell Hapney/HUDScript.cs
@@ -3,22 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//using their UI system
+using UnityEngine.UI;
+
 public class HUDScript : MonoBehaviour {
 
     float playerScore = 0;
+	//getting the UI text GameObject and setting it to a private
+	[SerializedField] Text playerScoreText;
 
 	// Update is called once per frame
 	void Update ()
-    {
-        playerScore += Time.deltaTime;	
+    	{
+		
+        	playerScore += Time.deltaTime;
+		//setting the value of the text(string) to the value of the float
+		//combined with whatever formatting you had
+		playerScoreText.text = "" + playerScore * 100 + "";
 	}
 
     public void IncreaseScore(int amount)
     {
         playerScore += amount;
     }
-    void OnGUI()
+	//while this is how you directly draw text to the screen, it isn't often used in production titles
+    /*void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)(playerScore * 100));
     }
+    */
 }
