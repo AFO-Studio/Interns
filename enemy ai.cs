69cea13edb1ecd39ee473e2240eb5dd6898a042b
Nicholas Pelfrey <pelfrey5@mctc.edu>
2018-04-20 10:57:14 -0400

    Add files via upload

---
+++ enemy ai.cs
@@ -0,0 +1,90 @@
+﻿using unityengine;
+using System.Collections;
+
+public class EnemyTerritory : MonoBehaviour
+{
+    public BoxCollider territory;
+    GameObject player;
+    bool playerInTerritory;
+
+    public GameObject enemy;
+    BasicEnemy basicenemy;
+
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        basicenemy = enemy.GetComponent<BasicEnemy>();
+        playerInTerritory = false;
+    }
+
+    
+    void Update()
+    {
+        if (playerInTerritory = true)
+        {
+            basicenemy.MoveToPlayer();
+        }
+
+        if (playerInTerritory = false)
+        {
+            basicenemy.Rest();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = false;
+        }
+    }
+}
+
+public class BasicEnemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
+    public float attack1Range = 1f;
+    public int attack1Damage = 1;
+    public float timeBetweenAttacks;
+
+
+    
+    void Start()
+    {
+        Rest();
+    }
+
+   
+    void Update()
+    {
+
+    }
+
+    public void MoveToPlayer()
+    {
+        
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        
+        if (Vector3.Distance(transform.position, target.position) > attack1Range)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    public void Rest()
+    {
+
+    }
+}
